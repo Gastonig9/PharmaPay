a5267a4e81747b6a05288f262cbec68cdc9b48fd
gastonig9 <gastonig2020@gmail.com>
2024-05-30 15:42:36 -0300

    Implementacion de loader

---
+++ src/components/base/Loader/Loaders.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import "./Loaders.css"
+
+export const Loaders = () => {
+  return (
+    <div className="loader-contain">
+        <div className="loader"></div>
+    </div>
+  )
+}
